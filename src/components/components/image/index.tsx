b2e3e786edc006ceb108108e9d0b7c6fa2ded60b
MiqayelChaloyan <miqochaloyan@gmail.com>
2024-09-22 20:22:16 +0400

    fix

--- src/components/components/image/index.tsx
+++ src/components/components/image/index.tsx
@@ -1,27 +1,29 @@
 'use client';
 
+import { ImgHTMLAttributes } from 'react';
 import Image from 'next/image';
 
-interface NextImageProps {
-  src: string;
-  alt: string;
-  width: number;
-  height: number;
-  className?: string;
+interface NextImageProps extends ImgHTMLAttributes<HTMLImageElement> {
+    src: string;
+    alt: string;
+    width: number;
+    height: number;
 };
 
-const NextImage = ({ src, alt, width, height, className }: NextImageProps) => {
-  return (
-    <Image
-      src={src}
-      alt={alt}
-      width={width}
-      height={height}
-      className={className}
-      loading="lazy"
-      quality={20}
-    />
-  );
+const NextImage = ({ src, alt= 'Image', width, height, className, ...rest }: NextImageProps) => {
+    return (
+        <Image
+            src={src}
+            alt={alt}
+            width={width}
+            height={height}
+            className={className}
+            quality={100}
+            priority
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            {...rest}
+        />
+    );
 };
 
 export default NextImage;
